652f00f6d4e94b7b14343cf43a37a2adfa7b2c85
Jimmy Carter <jcarter@marchex.com>
2017-01-04 12:57:25 -0800

    Update jerbs.groovy

--- cookbook_pipeline/jerbs.groovy
+++ cookbook_pipeline/jerbs.groovy
@@ -90,7 +90,7 @@ def all_the_jerbs(Map args) {
              from: 'tools-team@marchex.com',
              replyTo: 'tools-team@marchex.com',
              subject: "Jerbkins ${env.JOB_NAME} (${env.BUILD_NUMBER}) build failed",
-             to: 'jcarter@marchex.com'
+             to: 'jcarter@marchex.com,tflint@marchexcom,${env.CHANGE_AUTHOR}@marchex.com'
         throw err
     }
 }
